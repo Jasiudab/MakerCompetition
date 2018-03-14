38b0eb80f3ac0054b64221859fe13073d03808d3
Jasiudab <dabrowski.janek@gmail.com>
2018-03-14 17:33:32 +0000

    Adding appropriate scene for the multiplayer game

---
+++ Controllers/GameMultiController.java
@@ -0,0 +1,27 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import src.Player;
+
+import java.util.ArrayList;
+
+public class GameMultiController {
+
+    @FXML GridPane gridPane;
+    private ArrayList<PlayerRowController> playersControllers = new ArrayList<>();
+
+    public void initialize(ArrayList<Player> players) {
+        int row = 0;
+        for (Player elem : players) {
+
+            PlayerRowController playerRowController = new PlayerRowController();
+            playerRowController.initialize(elem, row+1);
+
+            playersControllers.add(playerRowController);
+
+            gridPane.add(playerRowController, 0, row);
+            row++;
+        }
+    }
+}
