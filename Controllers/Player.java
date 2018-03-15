e51276e0b057be671d143ce15f14e3999932be88
Jasiudab <dabrowski.janek@gmail.com>
2018-03-15 12:55:19 +0000

    Making the player comparable
    By score for sorting after each round

--- Controllers/Player.java
+++ Controllers/Player.java
@@ -1,19 +1,45 @@
 package Controllers;
 
-public class Player {
+public class Player implements Comparable<Player>  {
+
 	private int score;
 	private String name;
-	public Player(int score, String name){
+	private int position;
+
+	public Player(int score, String name) {
 		this.score = score;
-		this.name = name;
-		}
+	}
+
 	public void addScore(int newScore ){
 		score+=newScore;
 	}
+
 	public int getScore(){
 		return score;
 	}
+
 	public String getName(){
 		return name;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	@Override
+	public int compareTo(Player player) {
+		return Integer.compare(this.score, player.score);
 	}
 }
